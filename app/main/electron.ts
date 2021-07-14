c9425ddfc64e0d4f4ffbbebad846f3faeaca0663
linker <llin@junglescout.com>
2021-07-14 16:38:26 +0800

    [+] install redux-devtools

--- app/main/electron.ts
+++ app/main/electron.ts
@@ -1,4 +1,5 @@
 import path from 'path'
+import installExtension, { REDUX_DEVTOOLS } from 'electron-devtools-installer'
 import { app, BrowserWindow } from 'electron'
 
 function isDev() {
@@ -24,6 +25,11 @@ function createWindow() {
 }
 
 app.whenReady().then(() => {
+  if (isDev()) {
+    installExtension(REDUX_DEVTOOLS)
+      .then((name) => console.log(`Added Extension:  ${name}`))
+      .catch((err) => console.log('An error occurred: ', err))
+  }
   createWindow()
   app.on('activate', function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
