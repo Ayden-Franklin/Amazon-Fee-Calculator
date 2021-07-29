120929f9215d877d0da05674424ff396f0f482b5
linker <llin@junglescout.com>
2021-07-29 17:15:58 +0800

    [+] ca parser closing and referral

--- app/service/parser-ca.ts
+++ app/service/parser-ca.ts
@@ -137,5 +137,178 @@ export function parseDimensionalWeight(content: string) {
 
 export function parseFba() {}
 
-export function parseReferral() {}
-export function parseClosing() {}
+export function parseReferral(content: string, subContent?: StringRecord) {
+  const $ = cheerio.load(content)
+  let referralRule: IReferralFee[] = []
+
+  // for handle Baby Products (excluding Baby Apparel)
+  const parseCategory = (fullCategory: string): [string, Array<string>, Array<string>] => {
+    let excludingCategories: Array<string> = []
+    let includingCategories: Array<string> = []
+
+    const categoryMatchs = fullCategory.match(/\(.+\)/)
+
+    if (!categoryMatchs?.length) return [fullCategory, excludingCategories, includingCategories]
+
+    let splitCategoryIndex = categoryMatchs.index || -1
+    let realCategory = fullCategory.substring(0, splitCategoryIndex).trim()
+
+    for (const cI of categoryMatchs) {
+      if (cI.includes('excluding')) {
+        excludingCategories.push(
+          cI
+            .substring(1, cI.length - 1)
+            .replace('excluding', '')
+            .trim()
+        )
+        continue
+      }
+      if (cI.includes('including')) {
+        includingCategories.push(
+          cI
+            .substring(1, cI.length - 1)
+            .replace('including', '')
+            .trim()
+        )
+        continue
+      }
+    }
+    return [realCategory, excludingCategories, includingCategories]
+  }
+
+  $('tbody tr').each((_, tr) => {
+    const [nameEle, rateEle, miniFeeEle] = $(tr).find('td')
+    const [category, excludingCategories, includingCategories] = parseCategory(
+      $($(nameEle).contents().get(0)).text().trim() || $(nameEle).text()
+    )
+    referralRule.push({
+      category,
+      // TODO , need by country diff handle
+      otherable: ['Everything Else'].includes(category),
+      excludingCategories,
+      includingCategories,
+      // rangeItems: !rateOnlyOne ? parseReferralSubItem($(rateEle).toString()) : [],
+      rateItems: parseReferralSubItem($(rateEle).toString()),
+      minimumFee:
+        parseFloat(
+          $(miniFeeEle)
+            .text()
+            .replace(/CAD|\$/g, '')
+        ) || 0,
+    })
+  })
+
+  const specialReferralNames = ['Full-Size Appliances']
+  const specialReferralRules = referralRule.filter((r) => specialReferralNames.includes(r.category))
+
+  specialReferralRules.forEach((r) => {
+    // handle special referral
+    const mifyKey = r.category.replace(/-|\s/g, '').toLowerCase()
+    if (!mifyKey || !subContent || !subContent[mifyKey]) return
+    const parseSubContent = subContent[mifyKey]
+    // special TODO
+    if (mifyKey === 'fullsizeappliances') {
+      const $sub = cheerio.load(parseSubContent)
+      const [inc, exc] = $sub('tbody').toArray()
+
+      $sub(exc)
+        .find('tr')
+        .each((_1, tr) => {
+          const [_2, productTypeEle] = $(tr).find('td')
+          r.excludingCategories.push(
+            ...$sub(productTypeEle)
+              .text()
+              .split(',')
+              .map((ct) => ct?.trim())
+          )
+        })
+
+      $sub(inc)
+        .find('tr')
+        .each((_1, tr) => {
+          const [productTypeEle] = $(tr).find('td')
+          r.includingCategories.push($sub(productTypeEle).text())
+        })
+    }
+  })
+  return referralRule
+}
+
+function parseReferralSubItem(content: string) {
+  const $ = cheerio.load(content)
+  const rateItems: IReferralRateFeeItem[] = []
+  const onlyOneRate = $('ul').length === 0
+
+  if (onlyOneRate) {
+    const desc = $(content).text()
+    rateItems.push({
+      minPrice: 0,
+      maxPrice: Number.MAX_VALUE,
+      rate: parseFloat(desc) / 100,
+      desc,
+    })
+
+    return rateItems
+  }
+
+  /** PARSE Referral
+   * <td>
+   *  <ul>
+   *    <li>
+   *    ...
+   *    </li>
+   *  </ul>
+   * </td>
+   */
+  $(content)
+    .find('li')
+    .each((_, element) => {
+      const desc = $(element).find('span').text()
+      const array = desc.match(/(\d+(,\d+)*(\.\d*)?)/g)
+
+      if (array && array?.length > 1) {
+        let rate = parseInt(array[0], 10) / 100
+        let priceV1 = parseFloat(array[1].replaceAll(',', ''))
+        let priceV2 = parseFloat(array[2]?.replaceAll(',', ''))
+        // console.log('array', array, priceV1, priceV2)
+
+        const lastRate = rateItems.length && rateItems[rateItems.length - 1]
+        const minPrice = lastRate ? lastRate?.maxPrice : 0
+        const maxPrice = priceV2 || (minPrice === priceV1 || !priceV1 ? Number.MAX_VALUE : priceV1)
+
+        rateItems.push({
+          minPrice,
+          maxPrice,
+          rate,
+          desc,
+        })
+      }
+    })
+
+  return rateItems
+}
+export function parseClosing(content: Nullable<string>): IClosingRule[] {
+  if (!content) return []
+
+  const $ = cheerio.load(content)
+  const table = $('.help-content:eq(0)')
+  const p = $(table).find('div p')
+  const text = $(p).text()
+  const array = text.match(/\$\d+(\.\d*)?/)
+  const fee = array && array?.length > 0 ? array[0] : '$0'
+
+  // TODO
+  const coreStringKey = 'categories are'
+  const begin = text.indexOf(coreStringKey) + coreStringKey.length
+  const categoryNames = text.substring(begin) + 1
+  const names = categoryNames.split(', ')
+  if (names[names.length - 1].indexOf('and') > -1) {
+    names[names.length - 1] = names[names.length - 1].substring(4)
+  }
+  return [
+    {
+      categories: names,
+      fee: parseFloat(fee.substring(1)),
+    },
+  ]
+}
